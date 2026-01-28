cbd10000afc5b7d13b1e8aa1d1786ad00c69caef
nandhanas07 <nandhana072007@gmail.com>
2026-01-28 08:52:59 +0530

    problem_056 solved

--- problems/medium/problem_056.py
+++ problems/medium/problem_056.py
@@ -6,8 +6,8 @@ Difficulty: Medium
 
 def update_budget(current_val, input_val):
      
-    return current_val + input_val
+    return {current_val + input_val}
 
 curr = 100
-inp = "50"
+inp = 50
 print(update_budget(curr, inp))
